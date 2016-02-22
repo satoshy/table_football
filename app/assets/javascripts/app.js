654b1cc5eb4aa2f1020435b10c6accf3c12aee0a
satoshy <alex.stipanenko@gmail.com>
2016-02-22 08:41:27 +0200

    added signin and signup forms to angular

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,7 +6,8 @@ tf.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', { url: '/', templateUrl: 'home/index.html', controller: 'HomeCtrl' })
     .state('tournaments', { url: 'tournaments', templateUrl: 'tournaments/index.html', controller: 'TourCtrl' })
-    .state('auth', { url: 'auth/login', templateUrl: 'auth/login.html', controller: 'AuthCtrl' });
+    .state('auth_login', { url: '/auth/login', templateUrl: 'auth/login.html', controller: 'AuthCtrl' })
+    .state('auth_signup', { url: '/auth/signup', templateUrl: 'auth/signup.html', controller: 'AuthCtrl' });
 
    $urlRouterProvider.otherwise('/');
 });
--- app/assets/javascripts/controllers/AuthCtrl.js
+++ app/assets/javascripts/controllers/AuthCtrl.js
@@ -1,5 +1,5 @@
 angular.module('table_football')
-    .controller('HomeCtrl', function ($scope, $http) {
+    .controller('AuthCtrl', function ($scope, $http) {
       $http.get('http://localhost:3000/tournaments.json')
       .then(function(res){
         $scope.tournaments = res.data;
