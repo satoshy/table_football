ac6ed4293700b4425d0dd0e73c24eda63594a1ed
Kukhvan <kuharvanyusha@gmail.com>
2016-03-03 08:43:27 +0200

    signup app

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,9 @@ module TableFootball
     config.autoload_paths << Rails.root.join('lib')
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+
+    config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
   end
 end
