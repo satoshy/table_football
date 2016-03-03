d98c8b32e3da92e79ecb465679d8e788f274a91f
satoshy <alex.stipanenko@gmail.com>
2016-03-03 16:46:24 +0200

    fixed login and register with devise

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,14 +1,15 @@
 'use strict';
 
-var tf = angular.module('table_football', ['rails', 'ngAnimate', 'ui.router', 'templates', 'Devise']);
+var app = angular.module('table_football', ['rails', 'ngAnimate', 'ui.router', 'templates', 'Devise']);
 
-tf.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', { url: '/', templateUrl: 'home/index.html', controller: 'HomeCtrl' })
     .state('tournaments', { url: '/tournaments', templateUrl: 'tournaments/index.html', controller: 'TournamentCtrl' })
     .state('tournaments/new', { url: '/tournaments/new', templateUrl: 'tournaments/new.html', controller: 'TournamentCtrl' })
     .state('auth_login', { url: '/auth/login', templateUrl: 'auth/login.html', controller: 'AuthCtrl' })
-    .state('auth_signup', { url: '/auth/signup', templateUrl: 'auth/signup.html', controller: 'AuthCtrl' });
+    .state('auth_signup', { url: '/auth/signup', templateUrl: 'auth/signup.html', controller: 'AuthCtrl' })
+    .state('auth_logout', { url: '/auth/logout', templateUrl: '', controller: 'AuthCtrl' });
 
    $urlRouterProvider.otherwise('/');
 }]);
--- app/assets/javascripts/controllers/AuthCtrl.js
+++ app/assets/javascripts/controllers/AuthCtrl.js
@@ -1,9 +1,53 @@
-'use strict'
+'use strict';
 
 angular.module('table_football')
-    .controller('AuthCtrl', ['$scope', '$http', function ($scope, $http) {
-      $http.get('http://localhost:3000/tournaments.json')
-      .then(function(res){
-        $scope.tournaments = res.data;
-      });
-    }]);
+  .controller('AuthCtrl', ['$scope', '$location', 'Auth', function($scope, $location, Auth){
+  	$scope.user = {};
+
+    var config = {
+        headers: {
+          'X-HTTP-Method-Override': 'POST'
+        }
+     };
+
+  	$scope.signIn = function() {
+        Auth.login($scope.user, config).then(function(user) {
+            console.log(user); // => {id: 1, ect: '...'}
+        }, function(error) {
+            // Authentication failed...
+        });
+
+        $scope.$on('devise:login', function(event, currentUser) {
+          //hard regresh
+        });
+
+        $scope.$on('devise:new-session', function(event, currentUser) {
+            $location.path('/home/index');
+        });
+  	}
+
+  	$scope.signUp = function() {
+
+        Auth.register($scope.user, config).then(function(registeredUser) {
+            console.log(registeredUser); // => {id: 1, ect: '...'}
+        }, function(error) {
+            // Registration failed...
+        });
+
+        $scope.$on('devise:new-registration', function(event, user) {
+            $location.path('/home/index');
+        });
+  	}
+
+  	$scope.log_out = function() {
+  		Auth.logout(config).then(function(oldUser) {
+            // alert(oldUser.name + "you're signed out now.");
+        }, function(error) {
+            // An error occurred logging out.
+        });
+
+        $scope.$on('devise:logout', function(event, oldCurrentUser) {
+            $location.path('/home/index');
+        });
+  	}
+  }]);
